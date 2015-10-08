83524da402849a7e3475dbc67ae7de6351c5820f
lechuga <sautuja_14@hotmail.com>
2015-10-08 16:40:33 +0200

    quickSort

--- AktoreKatalogoa.java
+++ AktoreKatalogoa.java
@@ -1,5 +1,7 @@
 package lab1;
 
+import java.util.*;
+
 public class AktoreKatalogoa {
 	private ListaAktoreak lista;
 	private static AktoreKatalogoa nireAktoreKatalogoa;
@@ -15,5 +17,36 @@ public class AktoreKatalogoa {
 		  return nireAktoreKatalogoa;
 		  
 		 }
-
+	
+	public void quickSort(Aktorea[] taula){
+		quickSort(taula,0,taula.length-1);
+	}
+	
+	private void quickSort(Aktorea[] taulaBat,int hasiera,int bukaera){
+		if (bukaera-hasiera>0){
+			int indizeaZatiketa=zatiketa(taulaBat,hasiera,bukaera);
+			quickSort(taulaBat,hasiera,indizeaZatiketa-1);
+			quickSort(taulaBat,indizeaZatiketa-1,bukaera);
+		}
+	}
+	private int zatiketa(Aktorea[] taula,int i,int f){
+		Aktorea lag=taula[i];
+		int ezker=i;
+		int eskuin=f;
+		while(ezker<eskuin){
+			while(taula[ezker].compareTo(lag)<=0 && ezker<eskuin)
+				ezker++;
+			while(taula[eskuin].compareTo(lag)>0)
+				eskuin--;
+			if (ezker<eskuin)
+				swap(taula,ezker,eskuin);
+		}
+		taula[i]=taula[eskuin];
+		taula[eskuin]=lag;
+		return eskuin;
+		}
+	
 }
+
+
+
